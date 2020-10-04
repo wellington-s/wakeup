36c1a58310e25d046f7157c5b8c650f0db45d1af
Wellington <Wellingtonss150@gmail.com>
2020-10-04 15:48:30 -0300

    Começando com ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./pages/contato/contato.module').then( m => m.ContatoPageModule)
   }
 ];
 
